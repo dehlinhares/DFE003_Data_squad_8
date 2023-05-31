5bdceec72a9f75d26ed3ca680640906b17df1a64
DayanadoValle <isaque.dayana@gmail.com>
2023-05-31 18:13:01 -0300

    testes dayana

---
+++ src/main.js
@@ -0,0 +1,37 @@
+
+
+console.log("script funcionando")
+
+/*import data from "../src/data/disney/characters.json" assert { type: "json"};
+
+var dataListDisney = data
+var htmlShow = document.getElementById("content")
+var allHtmlShowInfos;
+
+function displayNames(data) {
+    allHtmlShowInfos = data.map ({{ name , imageUrl}} => {
+        return (allHtmlShowInfos =  
+            `<div class="card" id="card">
+            <div class="gridContainerUp" id="gridContainerUp">
+            <div class="title" id="title">${name}</div>
+            <div class="title" id="title">${imageUrl}</div>
+            </div>
+          </div>`)
+        });
+
+
+htmlShow.innerHTML = allHtmlShowInfos.join("")
+
+}
+
+displayNames(dataListDisney)*/
+
+
+/*teste2
+import data from './data/movies/movies.json' assert {type: "json"};
+
+var filmes = data
+console.log(filmes)  */
+
+
+
